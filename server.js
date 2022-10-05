6ecadc324da1188ed995419d71c35e3c8edb341b
Shanel Hickman-Davis <shanelhickmandavis@gmail.com>
2022-10-05 16:02:16 -0400

    My server is set up and running

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const PORT = 3000
+
+const app = express();
+
+app.get('/', function (req, res) {
+    res.send('Welcome to the Pokemon App');
+  });
+
+
+
+
+
+
+app.listen(3000, function () {
+    console.log('Listening on port 3000');
+  });
